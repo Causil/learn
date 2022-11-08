8939981a06293b2d8ebcd1d331cbf9e29fe460ca
Javier Andrés Causil Martínez <jcausilmartinez@gmail.com>
2022-11-08 11:35:37 -0500

    Organizando

---
+++ my_env/devnet.lab/parsexml.py
@@ -0,0 +1,9 @@
+import xml.etree.ElementTree as et
+import re
+
+xml = et.parse('myfile.xml')
+root = xml.getroot()
+ns = re.match ('{.*}', root.tag) .group (0)
+editconf = root.find "{} edit-config" .format (ns))
+defop = editconf.find («{} default-operation» .format (ns))
+testop = editconf.find («{} test-option» .format (ns))
